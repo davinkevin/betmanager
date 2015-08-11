0588f0eb40f9b239eb42231054959445377e8bff
Davin Kevin <davin.kevin@gmail.com>
2015-08-11 23:38:25 +0200

    feat(user): add service layer of userService

---
+++ src/main/java/com/github/davinkevin/betmanager/service/UserService.java
@@ -0,0 +1,32 @@
+package com.github.davinkevin.betmanager.service;
+
+import com.github.davinkevin.betmanager.entity.User;
+import com.github.davinkevin.betmanager.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+/**
+ * Created by kevin on 11/08/15 for betmanager
+ */
+@Service
+public class UserService {
+    final UserRepository userRepository;
+
+    @Inject
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findOne(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    public void delete(Long id) {
+        userRepository.delete(id);
+    }
+}
