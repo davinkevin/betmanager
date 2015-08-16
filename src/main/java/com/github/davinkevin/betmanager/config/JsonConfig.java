2872a48af052f8ce8b0500161486ac94667b4772
Davin Kevin <davin.kevin@gmail.com>
2015-08-16 13:21:03 +0200

    chore(json): add support of json serialization for date

---
+++ src/main/java/com/github/davinkevin/betmanager/config/JsonConfig.java
@@ -0,0 +1,29 @@
+package com.github.davinkevin.betmanager.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+/**
+ * Created by kevin on 16/08/15 for betmanager
+ */
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        return new MappingJackson2HttpMessageConverter(customObjectMapper());
+    }
+
+    private ObjectMapper customObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
+        objectMapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+
+        return objectMapper;
+    }
+}
